8054877af71678cc07b00fe20e262fce9d6969d7
nathaliechardon <nchardon22@gmail.com>
2023-02-16 16:26:37 -0800

    tried (and failed) to turn 0s to NAs where necessary

--- scripts/prep_biotime/data_processing.R
+++ scripts/prep_biotime/data_processing.R
@@ -62,7 +62,7 @@ length(unique(collated.pairs$STUDY_ID)) == length(unique(meta.pairs$STUDY_ID)) #
 
 # Create an exclusion dataframe
 
-ee <- c(334, 274) ##IN PROGRESS: need to add any studies to exclude from NC & HB & RL
+ee <- c(492, 334, 274) ##IN PROGRESS: need to add any studies to exclude from HB & RL (?)
 
 exclude <-  collated.pairs %>% 
   filter(STUDY_ID %in% ee) #filter for studies to exclude
@@ -85,6 +85,38 @@ collated.pairs %>%
 # # CONVERT FAKE 0s TO NA # # 
 
 ####################################################################################################
+
+# Problem: authors report they don’t measure abundance or biomass, but then put 0’s in that column 
+# Solution: change to NA with for loop
+
+summary(collated.pairs$sum.allrawdata.ABUNDANCE) #n = 0 NAs
+summary(collated.pairs$sum.allrawdata.BIOMASS) #n = 27141 NAs
+
+
+# Set up data to be used in for loop
+
+mm <- unique(meta.pairs$STUDY_ID) #unique study IDs from metadata
+
+
+# Loop through each metadata row, identify if either abundance or biomass was reported as NA, 
+# change to NA if listed as 0 in collated data
+
+for (i in 1:length(mm)) { #loop through each metadata row
+
+  if (is.na(meta.pairs$ABUNDANCE_TYPE[i])) { #if abundance type is listed as 'NA' in meta.pairs... 
+    
+    # ... turn all abundance values in collated.pairs to NA
+    collated.pairs[which (collated.pairs$STUDY_ID == mm[i]) , ]$sum.allrawdata.ABUNDANCE <- NA
+  }
+  
+  if (is.na(meta.pairs$BIOMASS_TYPE[i])) { #if biomass type is listed as 'NA' in meta.pairs... 
+    
+    # ... turn all biomass values in collated.pairs to NA
+    collated.pairs[which (collated.pairs$STUDY_ID == mm[i]) , ]$sum.allrawdata.BIOMASS <- NA
+  }
+}
+
+
 
 
 
