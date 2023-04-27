6ade7c15d2c5190306a06e5afea970f753a6fa0d
nathaliechardon <nchardon22@gmail.com>
2023-04-27 15:18:50 -0700

    prelim data exploration Q2

--- scripts/analyses_Q2/explore_Q2.R
+++ scripts/analyses_Q2/explore_Q2.R
@@ -10,6 +10,7 @@
 library(tidyverse)
 library(dplyr)
 library(brms)
+library(lattice)
 
 
 rm(list=ls()) 
@@ -32,4 +33,52 @@ dat <- readRDS('data/log.prop.change.with.meta.w.taxa.RDS')
 
 # Look at data
 dat <- readRDS('data/log.prop.change.with.meta.w.taxa.RDS')
+
+head(dat)
+
+unique(dat$REALM1)
+unique(dat$CLIMATE1)
+
+
+# Make sure CLIMATE is always listed as the same within pair
+foo <- dat %>% 
+  
+  mutate(mismatch = if_else(CLIMATE1 != CLIMATE2, 1, 0))
+
+unique(foo$mismatch) #CLIMATE always the same
+
+
+# Look at climate data
+xyplot(dat$Log.prop.change.abun.Gn1 ~ dat$Log.prop.change.abun.Gn2 | dat$CLIMATE1)
+
+# # Not much tropical data => climate a valuable fixed effect? 
+
+
+# Filter to temperate-terrestrial (where almost all data is)
+tt <- dat %>% 
+  filter(REALM1 == 'Terrestrial' | REALM2 == 'Terrestrial') %>% 
+  filter(CLIMATE1 == 'Temperate') 
+
+
+# Look at taxa pairs
+unique(tt$RESOLVED.TAXA1)
+unique(tt$RESOLVED.TAXA2)
+
+tt <- mutate(tt, taxa.pair = paste(RESOLVED.TAXA1, RESOLVED.TAXA2, sep = '_')) #create taxa pairs
+unique(tt$taxa.pair)
+
+# Taxa pairs for abundance
+abund.tt <- filter(tt, is.na(tt$Log.prop.change.abun.Gn1)!=TRUE & #filter out non-abund studies
+                     is.na(tt$Log.prop.change.abun.Gn2)!=TRUE)
+
+xyplot(abund.tt$Log.prop.change.abun.Gn1 ~ abund.tt$Log.prop.change.abun.Gn2 | abund.tt$taxa.pair, 
+       xlab = 'Abundance Change Genus 2', ylab = 'Abundance Change Genus 1')
+
+# Taxa pairs for biomass
+bio.tt <- filter(tt, is.na(tt$Log.prop.change.bio.Gn1)!=TRUE & #filter out non-biomass studies
+                     is.na(tt$Log.prop.change.bio.Gn2)!=TRUE)
+
+xyplot(bio.tt$Log.prop.change.bio.Gn1 ~ bio.tt$Log.prop.change.bio.Gn2 | bio.tt$taxa.pair, 
+       xlab = 'Biomass Change Genus 2', ylab = 'Biomass Change Genus 1')
+
 
