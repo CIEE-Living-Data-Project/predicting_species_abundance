2fed47869e608a03820b0f85fba81358ea4a4aa0
sdstraus <straus@zoology.ubc.ca>
2023-04-27 12:17:09 -0400

    pulled in files from Emily's branch

---
+++ scripts/data_processing/getting_pair_interactions_EB_26April2023.R
@@ -0,0 +1,175 @@
+# Date created: 26 Apr 2023
+# Date updated: 26 Apr 2023 (NC)
+
+# # LIBRARIES # #
+library(tidyverse)
+library("dplyr")
+library("tibble")
+library("readr") 
+library("ggplot2")
+library("magrittr")
+library('rglobi')
+library(progress)
+
+
+rm(list=ls()) 
+
+
+
+
+log.prop.change.with.meta <- readRDS("data/log.prop.change.with.meta.RDS")
+log_change <- log.prop.change.with.meta
+
+head(log.prop.change.with.meta)
+
+#list of bio pairs with at least 10 overlapping years
+genus_pairs <- apply(log_change[, c("Gn1", "Gn2")], 1, function(x) paste(sort(x), collapse = "-"))
+unique_pairs <- unique(genus_pairs)
+split_pairs <- data.frame(Gn1 = sapply(strsplit(unique_pairs, "-"), "[", 1),
+                          Gn2 = sapply(strsplit(unique_pairs, "-"), "[", 2))
+
+#short sample dataset: 
+# Split into 5 groups of roughly equal size
+groups <- cut(seq_along(1:12623), breaks = 5, labels = FALSE)
+
+# Split data into separate data frames
+df_split <- split(split_pairs, groups)
+names(df_split) <- paste0("split_", names(df_split))
+
+
+list2env(df_split, envir = .GlobalEnv)
+
+
+set.prog.bar<-function(n_iter){
+  #make progress bar
+  progress_bar$new(format = "(:spin) [:bar] :percent [Elapsed time: :elapsedfull || Estimated time remaining: :eta]",
+                   total = n_iter,
+                   complete = "=",
+                   incomplete = "-",
+                   current = ">",
+                   clear = FALSE,
+                   width = 100)
+  
+} #function to make a progress bar
+
+#Run for BETWEEN studies
+
+df_list <- list()
+
+
+pb<-set.prog.bar(nrow(split_1)) #sets progress bar
+for (i in 1:nrow(split_1)) {
+  pb$tick()
+  gn1 <- split_1[i, "Gn1"]
+  gn2 <- split_1[i, "Gn2"]
+interactions <- get_interactions_by_taxa(sourcetaxon=gn1, 
+                         targettaxon=gn2)
+unique <- unique(interactions$interaction_type)
+new_df <- data.frame(genus_1 = rep(gn1, each = length(unique)), 
+                  genus_2 = rep(gn2, each = length(unique)))
+pairs <- cbind(new_df, char = rep(unique, length.out = nrow(new_df)))
+df_list[[i]] <- pairs
+}
+
+pair_interactions_1 <- do.call(rbind, df_list)
+write.csv(pair_interactions_1, "data/pair_interactions_1.csv")
+
+
+
+
+
+pb<-set.prog.bar(nrow(split_2)) #sets progress bar
+for (i in 1:nrow(split_2)) {
+  pb$tick()
+  gn1 <- split_2[i, "Gn1"]
+  gn2 <- split_2[i, "Gn2"]
+  interactions <- get_interactions_by_taxa(sourcetaxon=gn1, 
+                                           targettaxon=gn2)
+  unique <- unique(interactions$interaction_type)
+  new_df <- data.frame(genus_1 = rep(gn1, each = length(unique)), 
+                       genus_2 = rep(gn2, each = length(unique)))
+  pairs <- cbind(new_df, char = rep(unique, length.out = nrow(new_df)))
+  df_list[[i]] <- pairs
+}
+
+pair_interactions_2 <- do.call(rbind, df_list)
+write.csv(pair_interactions_2, "data/pair_interactions_2.csv")
+
+
+
+
+pb<-set.prog.bar(nrow(split_3)) #sets progress bar
+for (i in 1:nrow(split_3)) {
+  pb$tick()
+  gn1 <- split_3[i, "Gn1"]
+  gn2 <- split_3[i, "Gn2"]
+  interactions <- get_interactions_by_taxa(sourcetaxon=gn1, 
+                                           targettaxon=gn2)
+  unique <- unique(interactions$interaction_type)
+  new_df <- data.frame(genus_1 = rep(gn1, each = length(unique)), 
+                       genus_2 = rep(gn2, each = length(unique)))
+  pairs <- cbind(new_df, char = rep(unique, length.out = nrow(new_df)))
+  df_list[[i]] <- pairs
+}
+
+pair_interactions_3 <- do.call(rbind, df_list)
+write.csv(pair_interactions_3, "data/pair_interactions_3.csv")
+
+
+
+pb<-set.prog.bar(nrow(split_4)) #sets progress bar
+for (i in 1:nrow(split_4)) {
+  pb$tick()
+  gn1 <- split_4[i, "Gn1"]
+  gn2 <- split_4[i, "Gn2"]
+  interactions <- get_interactions_by_taxa(sourcetaxon=gn1, 
+                                           targettaxon=gn2)
+  unique <- unique(interactions$interaction_type)
+  new_df <- data.frame(genus_1 = rep(gn1, each = length(unique)), 
+                       genus_2 = rep(gn2, each = length(unique)))
+  pairs <- cbind(new_df, char = rep(unique, length.out = nrow(new_df)))
+  df_list[[i]] <- pairs
+}
+
+pair_interactions_4 <- do.call(rbind, df_list)
+write.csv(pair_interactions_4, "data/pair_interactions_4.csv")
+
+
+
+
+
+pb<-set.prog.bar(nrow(split_5)) #sets progress bar
+for (i in 1:nrow(split_5)) {
+  pb$tick()
+  gn1 <- split_5[i, "Gn1"]
+  gn2 <- split_5[i, "Gn2"]
+  interactions <- get_interactions_by_taxa(sourcetaxon=gn1, 
+                                           targettaxon=gn2)
+  unique <- unique(interactions$interaction_type)
+  new_df <- data.frame(genus_1 = rep(gn1, each = length(unique)), 
+                       genus_2 = rep(gn2, each = length(unique)))
+  pairs <- cbind(new_df, char = rep(unique, length.out = nrow(new_df)))
+  df_list[[i]] <- pairs
+}
+
+pair_interactions_5 <- do.call(rbind, df_list)
+write.csv(pair_interactions_5, "data/pair_interactions_5.csv")
+
+all_interactions <- rbind(pair_interactions_1, pair_interactions_2, pair_interactions_3, 
+                          pair_interactions_4, pair_interactions_5)
+colnames(all_interactions) <- c("Gn1", "Gn2", "interaction")
+write.csv(all_interactions, "data/genus_interaction_list.csv")
+
+parasite <- all_interactions %>%
+  filter(interaction=="pathogenOf")
+
+#Remove interaction pairs that are mistakes (pathogen, host, hemiparasite
+#- see original all interactions above)
+
+mistake_interactions <- c("hasHost", "pathogenOf", "hemiparasiteOf", 'hostOf', 'pathogenOf')
+all_interactions_2<- all_interactions %>%
+  filter(!interaction %in% mistake_interactions)
+write.csv(all_interactions_2, "data/genus_interaction_list.csv")
+
+
+
