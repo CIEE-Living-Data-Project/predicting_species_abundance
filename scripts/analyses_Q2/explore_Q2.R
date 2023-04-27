a5c63fd9793c2a90b971e7c47bbf07f0fe5bfe24
nathaliechardon <nchardon22@gmail.com>
2023-04-27 13:06:52 -0700

    created data expl script in new folder

---
+++ scripts/analyses_Q2/explore_Q2.R
@@ -0,0 +1,32 @@
+# Aims:
+# 1. Explore approaches to answer Q2
+
+# Author: Nathalie Chardon
+# Date created: 27 April 2023
+# Date updated: 26 April 2023 (NC)
+
+
+# # LIBRARIES # #
+library(tidyverse)
+library(dplyr)
+library(brms)
+
+
+rm(list=ls()) 
+
+
+# # INPUT FILES # #
+dat <- readRDS('data/log.prop.change.with.meta.w.taxa.RDS')
+
+
+# # OUTPUT FILES # #
+
+
+
+
+####################################################################################################
+
+# # DATA EXPLORATION # # 
+
+####################################################################################################
+
