68cac391a52f7a2cd5fb0e96d9b14d8bdfa5ef1a
cour10eygrace <courtneygrace125@gmail.com>
2023-04-27 15:32:32 -0700

    updated lmm script for q1

--- scripts/analyses_Q1/prelim_LMM.R
+++ scripts/analyses_Q1/prelim_LMM.R
@@ -12,17 +12,15 @@
 library(tidyverse)
 library(dplyr)
 library(brms)
-
+library(MCMCvis)
 
 rm(list=ls()) 
 
 
 # # INPUT FILES # #
-dat <- readRDS('data/dummy.dataset.RDS')
+#dat <- readRDS('data/dummy.dataset.RDS')
 
 # # OUTPUT FILES # #
-
-
 
 
 ####################################################################################################
@@ -32,59 +30,33 @@ dat <- readRDS('data/dummy.dataset.RDS')
 ####################################################################################################
 
 # Data
-dat <- readRDS('data/dummy.dataset.RDS')
-  
+#dat <- readRDS('data/dummy.dataset.RDS') #dummy data 
+
+dat <- readRDS('data/preprocessing/log.prop.change.with.meta.RDS')#full cleaned 
+names(dat)
+
+#subset to terrestrial and marine realms 
+dat_terr<-subset(x = dat, subset = REALM1=="Terrestrial" & REALM2=="Terrestrial")
+
+dat_marine<-subset(x = dat, subset = REALM1!="Terrestrial" & REALM2!="Terrestrial")
+
 FAM <- gaussian(link = 'identity')
 
-MODFORM <- brms::bf(Log.prop.change.abun.Gn1 ~ Log.prop.change.bio.Gn2 + #intercept + fixed effect
+MODFORM <- bf(Log.prop.change.abun.Gn1 ~ Log.prop.change.abun.Gn2 + #intercept + fixed effect
                       
-                      (Log.prop.change.bio.Gn2 | SERIES.length) + #rand slopes for time series length
+            (Log.prop.change.abun.Gn2 | SERIES.l) + #rand slopes for time series length
                       
-                      (Log.prop.change.bio.Gn2 | PairID/UNIQUE.PAIR.ID))   
-                      #rand slopes for genus[i]-genus[j] nested within studyID[i]-studyID[j]
+            (Log.prop.change.abun.Gn2 | PairID)+    
+            
+            (Log.prop.change.abun.Gn2 | UNIQUE.PAIR.ID))   
 
+#rand slopes for genus[i]-genus[j] nested within studyID[i]-studyID[j]
 
 # Fit full model
-
-mod <- brms::brm(MODFORM, data = dat, family = FAM, seed = 042023, #set seed
+mod<-brm(MODFORM, data = dat_terr, family = FAM, seed = 042023, #set seed
+         control = list(adapt_delta=0.99, max_treedepth = 12),    
+                   chains = 3, iter = 5000, warmup = 1000, cores = 4) #fitting information
                  
-                    chains = 3, iter = 5000, warmup = 1000, cores = 4, #fitting information
-                 
-                    file = 'outputs/brms_April2023/dummy_mod.rds', file_refit = 'on_change') #save
+save(mod, file = 'outputs/brms_April2023/full_mod_q1.rds') #save
 
-# # FITTING ISSUES
-
-# NA's -> these are biomass values
-
-# divergent transitions -> NEED TO DEAL WITH THIS (http://mc-stan.org/misc/warnings.html#divergent-transitions-after-warmup)
-# low ESS -> NEED TO DEAL WITH THIS
-
-coefs <- coef(mod)$`PairID:UNIQUE.PAIR.ID`[1:52, 1, 2] #random int & slopes
-
-hist(coefs, breaks = 100)
-
-
-
-### NOT RUN; STOP 26.4.2023 NC
-
-# Check posterior
-summary(mod)
-plot(mod)
-plot(conditional_effects(mod))
-
-# Check prior
-prior_summary(mod) 
-ps <- powerscale_sensitivity(mod)
-unique(ps$sensitivity$diagnosis)
-
-# Model fit
-pp_check(mod, ndraws = 100)
-ppc_stat(y = dat$rel_rec, 
-         yrep = posterior_predict(mod, ndraws = 1000), stat = "skew")
-model_loo <- loo(mod, save_psis = TRUE, cores=4) 
-w <- weights(model_loo$psis_object)
-ppc_loo_pit_overlay(dat$rel_rec, 
-                    posterior_predict(mod), lw = w)
-
-
-
+summary(mod)#look at model outputs
