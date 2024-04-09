05c6c349f603a8d25d9dd3ca190af07076c11aaf
Mia T Waters <miatwaters@gmail.com>
2024-04-08 20:09:13 -0700

    updated, starting run now!

--- Revision 1 ecography/scripts/meta model analyses/mia_multithreading_tempscript.R
+++ Revision 1 ecography/scripts/meta model analyses/mia_multithreading_tempscript.R
@@ -1,7 +1,10 @@
 #Mia's changes to run_brms_metamodel.R April 3 2024
 #I'm only putting the code I altered/added here. All the other code in that script I ran as needed before each of these changes.
 
-
+## set priors ####
+priors <- c(prior(normal(0,0.33),class=b),
+            prior(normal(0,0.33),class=Intercept),
+            prior(cauchy(0,0.5), class = sd))
 
 
 #Mia trying full data:
@@ -30,8 +33,8 @@ cmdstanr::check_cmdstan_toolchain(fix = TRUE)
 #mia made changes to this model, including changiing to moddat below.
 metamod.sp <- brm(MODFORM.sp, moddat,cores=3, chains=3, backend = "cmdstanr", threads = threading(6), prior = priors, 
                   # control = list(adapt_delta=0.8, max_treedepth = 12), 
-                  iter=5000, warmup = 4000, family=gaussian,
-                  file="Revision 1 ecography/output/meta model/SEspecies_full5k_miathreads.rmd")
+                  iter=10000, warmup = 5000, thin = 5, family=gaussian,
+                  file="Revision 1 ecography/output/meta model/SEspecies_full5k_miathreads2.rmd")
 end_time <- Sys.time()
 end_time - start_time
 summary(metamod.sp)
