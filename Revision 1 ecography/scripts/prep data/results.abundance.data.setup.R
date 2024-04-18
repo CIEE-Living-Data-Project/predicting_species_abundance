b033b729a30d74af75c183ed2aecd23ae4529085
Emily Black <black.emily.nicole@gmail.com>
2024-04-18 11:30:04 -0700

    Commented out and renamed results.abundance generation script with fixes (studies 39 and 221) and richness/sample size

--- Revision 1 ecography/scripts/prep data/results.abundance.data.setup.R
+++ Revision 1 ecography/scripts/prep data/results.abundance.data.setup.R
@@ -1,3 +1,18 @@
+# Biotime cleaning, aggregating, and change in abundance calculations
+# Studies 39 and 221 corrected (note that 225 still contains errors)
+#Studies 39, 221, 225 ultimately removed from final analyses 
+# Date created: Jan 24 2024
+# Date updated: 18 April 2024
+
+
+
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+
+
+
+
+
 #make new biotime data for the WG 
 library(dplyr)
 library(progress)
@@ -9,12 +24,15 @@ metadata<-read.csv("~/Documents/Work and Career/LDP/Working Group/BioTIMEMetadat
 #remove marine and aquatic (keep only terr)
 biotime.t<-biotime[which(biotime$STUDY_ID%in%metadata$STUDY_ID[which(metadata$REALM=="Terrestrial")]),]
 
-#Fix study 39 before anything else
-#Study 39
+###### ENB addition - cleaning studies 39, 221 #######
+#Fix study 39 before cleaning, aggregation
+#Get only study 39
 study_39 <- biotime.t %>%
   filter(STUDY_ID == "39")
+#Summarize unique genera and species 
 unique(study_39$GENUS_SPECIES)
 unique(study_39$GENUS)
+#Correct erroneous species names 
 study_39$GENUS[study_39$GENUS_SPECIES == "Yellow-throated Warbler"] <- "Setophaga"
 study_39$GENUS[study_39$GENUS_SPECIES == "Black-capped chickadee"] <- "Poecile"
 study_39$GENUS[study_39$GENUS_SPECIES == "Blue jay"] <- "Cyanocitta"
@@ -33,20 +51,19 @@ study_39$GENUS[study_39$GENUS_SPECIES == "Swainsons Thrush"] <- "Catharus"
 study_39$GENUS[study_39$GENUS_SPECIES == "Yellow-bellied Sapsucker"] <- "Sphyrapicus"
 study_39$GENUS[study_39$GENUS_SPECIES == "Solitary Vireo"] <- "Vireo"
 study_39$GENUS[study_39$GENUS_SPECIES == "Red-breasted nuthatch"] <- "Sitta"
-#Add back into biotime
+#Add back into full biotime dataset
 biotime.t <- biotime.t %>%
   filter(!STUDY_ID == "39")
 biotime.t <- bind_rows(biotime, study_39)
 biotime.t <- biotime.t %>%
   arrange(STUDY_ID)
 
-#Get study 221
-
+#Correct study 221
+#Filter to only study 221
 study_221 <- biotime.t %>%
   filter(STUDY_ID == "221")
+#Summarize unique genera 
 unique(study_221$GENUS)
-#ChatGPT and research has provided genus guesses for each code, 
-#in the context of this being a NA tree/shrub study 
 #Reclassify the genera
 map_genus <- function(code) {
   ifelse(grepl("SAL", code), "Salix", 
@@ -64,18 +81,19 @@ map_genus <- function(code) {
                                                                                       code))))))))))))
 }
 study_221 <- study_221 %>% mutate(GENUS= map_genus(GENUS))
+#Summarize unique and corrected genera
 table(study_221$GENUS)
 #Add back into biotime.t
 biotime.t <- biotime.t %>%
   filter(!STUDY_ID=="221")
 biotime.t <- bind_rows(biotime.t, study_221)
-#Reorder
+#Reorder to ascending study ID
 biotime.t <- biotime.t %>%
   arrange(STUDY_ID)
 
 
 
-
+##### Back to IE code ####
 
 #any false 0s?
 table(biotime.t$sum.allrawdata.ABUNDANCE[which(biotime.t$STUDY_ID%in%metadata$STUDY_ID[which(is.na(metadata$ABUNDANCE_TYPE))])])
@@ -124,18 +142,10 @@ biotime.agg.abundance$STUDY_PLOT<-paste(biotime.agg.abundance$STUDY_ID,biotime.a
 #biotime.agg.biomass$STUDY_PLOT<-paste(biotime.agg.biomass$STUDY_ID,biotime.agg.biomass$PLOT,sep="~")
 
 
-########Emily's addition - fix genera names ##################
-
-
-#Make a new frame with only study 39 and 221 
-study_id_select <- c("39", "221")
-study_221_39 <- biotime.agg.abundance %>%
-  filter(STUDY_ID %in% study_id_select)
-
 #find genera-genera pairs (within studies) that overlap for 10 years
 find.genera.pairs.overlap.abundance<-function(study_plot){
   
-  cropped<-study_221_39[which(study_221_39$STUDY_PLOT==study_plot),]
+  cropped<-biotime.agg.abundance[which(biotime.agg.abundance$STUDY_PLOT==study_plot),]
 
   #get genera list
   genera <- cropped$GENUS %>% unique()
@@ -174,47 +184,8 @@ find.genera.pairs.overlap.abundance<-function(study_plot){
   return(pairs)
   
 }#abundance one
-# find.genera.pairs.overlap.biomass<-function(study_plot){
-#   
-#   cropped<-biotime.agg.biomass[which(biotime.agg.biomass$STUDY_PLOT==study_plot),]
-#   
-#   #get genera list
-#   genera <- cropped$GENUS %>% unique()
-#   genera = genera[!is.na(genera)]
-#   
-#   if (length(genera)<2){return(NULL)} #if not enough genera found exit the function
-#   
-#   pairs<-as.data.frame(t(apply(combn(genera,2),2,paste))) #get all unique pairs
-#   names(pairs)=c("Gn1","Gn2")
-#   pairs$STUDY_PLOT=study_plot
-#   pairs$STUDY_ID=unique(cropped$STUDY)
-#   pairs$PLOT=unique(cropped$PLOT)
-#   pairs$Max.Overlap<-NA
-#   pairs$Type="Within"
-#   
-#   for (i in 1:nrow(pairs)){
-#     
-#     gen1=pairs$Gn1[i]
-#     gen2=pairs$Gn2[i]
-#     
-#     #get overlapping years
-#     years1=cropped$YEAR[which(cropped$GENUS==gen1)]
-#     years2=cropped$YEAR[which(cropped$GENUS==gen2)]
-#     
-#     cont.overlapping<-split(years1[which(years1%in%years2)], cumsum(c(1, diff(years1[which(years1%in%years2)]) != 1)))
-#     
-#     dat<-data.frame("Period"=NA,"value"=NA)
-#     for (y in 1:length(cont.overlapping)){
-#       dat[y,1]=y
-#       dat[y,2]=length(cont.overlapping[[y]])}
-#     
-#     pairs$Max.Overlap[i]=max(dat$value)
-#     
-#   }
-#   
-#   return(pairs)
-#   
-# }#biomass one (NOT RUN)
+
+
 
 #run loop to calc all pairs
 set.prog.bar<-function(n_iter){
@@ -230,37 +201,25 @@ set.prog.bar<-function(n_iter){
 } #function to make a progress bar
 
 #Run abundance
-pb<-set.prog.bar(length(unique(study_221_39$STUDY_PLOT))) #sets progress bar
+pb<-set.prog.bar(length(unique(biotime.agg.abundance$STUDY_PLOT))) #sets progress bar
 within.studies.overlap.abundance=data.frame(matrix(ncol=7,nrow=0, dimnames=list(NULL, c("Gn1", "Gn2","STUDY_PLOT","STUDY_ID","PLOT","Max.Overlap","Type")))) #makes an empty dataframe
-for (h in 1:length(unique(study_221_39$STUDY_PLOT))){
+for (h in 1:length(unique(biotime.agg.abundance$STUDY_PLOT))){
   pb$tick()
-  study_plot=unique(study_221_39$STUDY_PLOT)[h]
+  study_plot=unique(biotime.agg.abundance$STUDY_PLOT)[h]
   data.pair<-find.genera.pairs.overlap.abundance(study_plot)
   within.studies.overlap.abundance<-rbind(within.studies.overlap.abundance,data.pair)
   
 } #populates dataframe with pairs
 
-# #Run biomass
-# pb<-set.prog.bar(length(unique(biotime.agg.biomass$STUDY_PLOT))) #sets progress bar
-# within.studies.overlap.biomass=data.frame(matrix(ncol=7,nrow=0, dimnames=list(NULL, c("Gn1", "Gn2","STUDY_PLOT","STUDY_ID","PLOT","Max.Overlap","Type")))) #makes an empty dataframe
-# for (h in 1:length(unique(biotime.agg.biomass$STUDY_PLOT))){
-#   pb$tick()
-#   study_plot=unique(biotime.agg.biomass$STUDY_PLOT)[h]
-#   data.pair<-find.genera.pairs.overlap.biomass(study_plot)
-#   within.studies.overlap.biomass<-rbind(within.studies.overlap.biomass,data.pair)
-#   
-# } #populates dataframe with pairs
+
 
 #make lists of final genera pairs and their studies to use 
 final.pairs.abundance<-within.studies.overlap.abundance[which(within.studies.overlap.abundance$Max.Overlap>9),c(3,1,2,4)]
-# final.pairs.biomass<-within.studies.overlap.biomass[which(within.studies.overlap.biomass$Max.Overlap>9),c(3,1,2,4)]
 
 dim(final.pairs.abundance)
-# dim(final.pairs.biomass)
 
 #add in some time series IDs
 final.pairs.abundance$TS_ID<-paste(final.pairs.abundance$Gn1,final.pairs.abundance$Gn2,final.pairs.abundance$STUDY_PLOT,sep="~")
-# final.pairs.biomass$TS_ID<-paste(final.pairs.biomass$Gn1,final.pairs.biomass$Gn2,final.pairs.biomass$STUDY_PLOT,sep="~")
 
 #calculate prop change for final lists
 get.log.prop.change.abundance<-function(ts){
@@ -272,7 +231,7 @@ get.log.prop.change.abundance<-function(ts){
   #get study
   study_plot=final.pairs.abundance$STUDY_PLOT[which(final.pairs.abundance$TS_ID==ts)]
   
-  cropped=study_221_39[which(study_221_39$STUDY_PLOT==study_plot & study_221_39$GENUS%in%c(gen1,gen2)),]
+  cropped=biotime.agg.abundance[which(biotime.agg.abundance$STUDY_PLOT==study_plot & biotime.agg.abundance$GENUS%in%c(gen1,gen2)),]
 
   #get years
   years1=cropped$YEAR[which(cropped$GENUS==gen1)]
@@ -378,105 +337,7 @@ get.log.prop.change.abundance<-function(ts){
     res$SERIES.l=nrow(res)+1
     
     return(res)}}
-#get.log.prop.change.biomass<-function(ts){
-  # 
-  # #get genera
-  # gen1=final.pairs.biomass$Gn1[which(final.pairs.biomass$TS_ID==ts)]
-  # gen2=final.pairs.biomass$Gn2[which(final.pairs.biomass$TS_ID==ts)]
-  # 
-  # #get study
-  # study_plot=final.pairs.biomass$STUDY_PLOT[which(final.pairs.biomass$TS_ID==ts)]
-  # 
-  # cropped=biotime.agg.biomass[which(biotime.agg.biomass$STUDY_PLOT==study_plot & biotime.agg.biomass$GENUS%in%c(gen1,gen2)),]
-  # 
-  # #get years
-  # years1=cropped$YEAR[which(cropped$GENUS==gen1)]
-  # years2=cropped$YEAR[which(cropped$GENUS==gen2)]
-  # 
-  # cont.overlapping<-split(years1[which(years1%in%years2)], cumsum(c(1, diff(years1[which(years1%in%years2)]) != 1)))
-  # 
-  # dat<-data.frame("Period"=NA,"value"=NA)
-  # for (y in 1:length(cont.overlapping)){
-  #   dat[y,1]=y
-  #   dat[y,2]=length(cont.overlapping[[y]])}
-  # 
-  # years.overlap=cont.overlapping[[dat$Period[which(dat$value==max(dat$value))]]] #these are the overlapping years
-  # 
-  # n=dat$Period[which(dat$value>9)] #this is the period(s) to isolate
-  # 
-  # if (length(n)>1){
-  #   res=data.frame(matrix(ncol=12,nrow=0, dimnames=list(NULL, c("TS_ID","STUDY_PLOT","Gn1", "Gn2", "Log.prop.change.Gn1","Log.prop.change.Gn2","SERIES.n","SERIES.start","SERIES.end","YEAR.T","YEAR.T1","METRIC")))) #makes an empty dataframe
-  #   for (x in 1:length(n)){
-  #     years.overlap=cont.overlapping[[n[x]]]
-  #     
-  #     start.year=years.overlap[1]
-  #     end.year=years.overlap[length(years.overlap)]
-  #     
-  #     #get measures
-  #     gen1.abun<-cropped$sum.allrawdata.BIOMASS[which(cropped$GENUS==gen1 & cropped$YEAR%in%years.overlap)]
-  #     gen2.abun<-cropped$sum.allrawdata.BIOMASS[which(cropped$GENUS==gen2 & cropped$YEAR%in%years.overlap)]
-  #     
-  #     #get year t and t+1
-  #     year.Ts=years.overlap[1:(length(years.overlap)-1)]
-  #     year.T1s=years.overlap[2:length(years.overlap)]
-  #     
-  #     #calc  change in abun
-  #     N1_gn1_abun = gen1.abun[2:length(gen1.abun)]
-  #     N0_gn1_abun = gen1.abun[1:length(gen1.abun)-1]
-  #     
-  #     N1_gn2_abun = gen2.abun[2:length(gen2.abun)]
-  #     N0_gn2_abun = gen2.abun[1:length(gen2.abun)-1]
-  #     
-  #     log_prop_change_gn1_abun = log(N1_gn1_abun/N0_gn1_abun)
-  #     log_prop_change_gn2_abun = log(N1_gn2_abun/N0_gn2_abun)
-  #     
-  #     
-  #     res.add<-data.frame("TS_ID"=ts,"STUDY_PLOT"=study_plot,"Gn1"=gen1,"Gn2"=gen2,
-  #                         "Log.prop.change.Gn1"=log_prop_change_gn1_abun,"Log.prop.change.Gn2"=log_prop_change_gn2_abun,
-  #                         "SERIES.n"=x,"SERIES.start"=start.year,"SERIES.end"=end.year,
-  #                         "YEAR.T"=year.Ts,"YEAR.T1"=year.T1s,"METRIC"="BIOMASS")
-  #     
-  #     res<-rbind(res,res.add)
-  #   }
-  #   
-  #   res$SERIES.l=nrow(res)+1
-  #   return(res)
-  #   
-  #   
-  # } #if multiple periods overlap at least 10 years calc for both!
-  # 
-  # if (length(n)==1){
-  #   years.overlap=cont.overlapping[[n]]
-  #   
-  #   start.year=years.overlap[1]
-  #   end.year=years.overlap[length(years.overlap)]
-  #   
-  #   #get abundances or biomass
-  #   gen1.abun<-cropped$sum.allrawdata.BIOMASS[which(cropped$GENUS==gen1 & cropped$YEAR%in%years.overlap)]
-  #   gen2.abun<-cropped$sum.allrawdata.BIOMASS[which(cropped$GENUS==gen2 & cropped$YEAR%in%years.overlap)]
-  #   
-  #   #get year t and t+1
-  #   year.Ts=years.overlap[1:(length(years.overlap)-1)]
-  #   year.T1s=years.overlap[2:length(years.overlap)]
-  #   
-  #   #calc  change in abun
-  #   N1_gn1_abun = gen1.abun[2:length(gen1.abun)]
-  #   N0_gn1_abun = gen1.abun[1:length(gen1.abun)-1]
-  #   
-  #   N1_gn2_abun = gen2.abun[2:length(gen2.abun)]
-  #   N0_gn2_abun = gen2.abun[1:length(gen2.abun)-1]
-  #   
-  #   log_prop_change_gn1_abun = log(N1_gn1_abun/N0_gn1_abun)
-  #   log_prop_change_gn2_abun = log(N1_gn2_abun/N0_gn2_abun)
-  #   
-  #   res<-data.frame("TS_ID"=ts,"STUDY_PLOT"=study_plot,"Gn1"=gen1,"Gn2"=gen2,
-  #                   "Log.prop.change.Gn1"=log_prop_change_gn1_abun,"Log.prop.change.Gn2"=log_prop_change_gn2_abun,
-  #                   "SERIES.n"=1,"SERIES.start"=start.year,"SERIES.end"=end.year,
-  #                   "YEAR.T"=year.Ts,"YEAR.T1"=year.T1s,"METRIC"="BIOMASS")
-  #   
-  #   res$SERIES.l=nrow(res)+1
-  #   
-  #   return(res)}}
+
 
 #loop through all abundance 
 results.abundance=data.frame(matrix(ncol=21,nrow=0, dimnames=list(NULL, c("TS_ID","STUDY_PLOT","Gn1", "Gn2", "Log.prop.change.Gn1","Log.prop.change.Gn2","SERIES.n","SERIES.start","SERIES.end","YEAR.T","YEAR.T1","METRIC","Abs.change.Gn1","Abs.change.Gn2","Abun.T.Gn1","Abun.T1.Gn1","Abun.T.Gn2","Abun.T1.Gn2","Length.Unique.Values.Gn1","Length.Unique.Values.Gn2","SERIES.l")))) #makes an empty dataframe
@@ -490,24 +351,43 @@ for (i in 1:nrow(final.pairs.abundance)){
   
 } #get prop change for every genera pair
 
-write.csv(results.abundance,"~/Documents/Work and Career/LDP/Working Group/results.abundance_221_29.csv")
-results.abundance=read.csv("~/Documents/Work and Career/LDP/Working Group/results.abundance_221_39.csv")
+#Note: save locations should be updated if running code on own device 
+#write.csv(results.abundance,"~/Documents/Work and Career/LDP/Working Group/results.abundance.csv")
+#results.abundance=read.csv("~/Documents/Work and Career/LDP/Working Group/results.abundance.csv")
 
-#Read in the study 221_39 original 
+
+
+
+
+
+
+###### ENB addition to code: calculating richness for studies with incorrect names ####
+#Note: right now only calculates richness for corrected studies 39 and 221
+#Initially calculated richness for all studies, written by IE
+#Can be easily adjusted to work for all studies
+#Current data used for analyses has richness and sample size for all studies, 
+#Including corrected 39 and 221 
+
+
+#Read in full biotime data
 biotime<-read.csv("~/Documents/Work and Career/LDP/Working Group/BioTIMEQuery_24_06_2021.csv")
+#Not available in github - subset of complete data that was shared during analysis. 
+#Corrected results available in Github
 missing_data <- read.csv("~/Documents/Work and Career/LDP/Working Group/data_missing_richness.csv")
+#Read in biotime metadata
 metadata<-read.csv("~/Documents/Work and Career/LDP/Working Group/BioTIMEMetadata_24_06_2021.csv")
 
 #remove marine and aquatic (keep only terr)
 biotime.t<-biotime[which(biotime$STUDY_ID%in%metadata$STUDY_ID[which(metadata$REALM=="Terrestrial")]),]
 
-
+#Filter biotime to only studies 39 and 221 (add 225 if using full data)
 studies <- c("39", "221")
 b_221_39 <- biotime.t %>%
   filter(STUDY_ID %in% studies)
+#Get list of unique species names 
 unique(b_221_39$GENUS_SPECIES)
 
-#Correct the genus and species names for study 39
+#Correct the genus and species names for study 39, as above
 name_mapping <- list(
   "SALIBEBB" = "Salix bebbiana",
   "POPUTRE2" = "Populus tremuloides",
@@ -575,10 +455,9 @@ convert_names <- function(x) {
   return(x)
 }
 
-
-# Applying the conversion function to the dataframe column
+# Apply convert_names to studies
 b_221_39$GENUS_SPECIES <- convert_names(b_221_39$GENUS_SPECIES)
-
+#Check new list of unique names 
 unique(b_221_39$GENUS_SPECIES)
 
 #Convert bird names 
@@ -644,11 +523,11 @@ convert_bird_names <- function(x) {
   return(x)
 }
 
-# Applying the conversion function to the vector
+# Apply function to studies 39 and 221 to fix names 
 b_221_39$GENUS_SPECIES <- convert_bird_names(b_221_39$GENUS_SPECIES)
 unique(b_221_39$GENUS_SPECIES)
 
-#Make new genus and species columns 
+#Make new genus and species columns with corrected names 
 genus_species <- strsplit(b_221_39$GENUS_SPECIES, " ")
 b_221_39$GENUS <- sapply(genus_species, `[`, 1)
 b_221_39$SPECIES <- sapply(genus_species, `[`, -1)
@@ -660,19 +539,21 @@ b$UNIQUE_ID<-paste(b$STUDY_ID,b$PLOT,b$GENUS,b$YEAR,sep="~")
 b$UNIQUE_ID_SP<-paste(b$UNIQUE_ID,b$SPECIES,sep="~")
 b<-b[-which(duplicated(b$UNIQUE_ID_SP)),]
 
+#Make table of unique IDs
 data<-as.data.frame(table(b$UNIQUE_ID))
 names(data)=c("UNIQUE_ID","Richness")
 
+#Replace parts of study_plot names so data can talk to each other 
 missing_data<- missing_data %>%
   mutate(STUDY_PLOT = str_replace_all(STUDY_PLOT, "Was_NA", "NA"))
 
-#add cols
+#add richness cols
 missing_data$Rich.T.Gn1=data$Richness[match(paste(missing_data$STUDY_PLOT,missing_data$Gn1,missing_data$YEAR.T,sep="~"),data$UNIQUE_ID)]
 missing_data$Rich.T1.Gn1=data$Richness[match(paste(missing_data$STUDY_PLOT,missing_data$Gn1,missing_data$YEAR.T1,sep="~"),data$UNIQUE_ID)]
 missing_data$Rich.T.Gn2=data$Richness[match(paste(missing_data$STUDY_PLOT,missing_data$Gn2,missing_data$YEAR.T,sep="~"),data$UNIQUE_ID)]
 missing_data$Rich.T1.Gn2=data$Richness[match(paste(missing_data$STUDY_PLOT,missing_data$Gn2,missing_data$YEAR.T1,sep="~"),data$UNIQUE_ID)]
 
-#add total sample size
+#add total sample size cols
 ss_abun<-as.data.frame(table(b$UNIQUE_ID))
 names(ss_abun)=c("UNIQUE_ID","Sample.Size")
 missing_data$SS.T.Gn1=data$Richness[match(paste(missing_data$STUDY_PLOT,missing_data$Gn1,missing_data$YEAR.T,sep="~"),data$UNIQUE_ID)]
@@ -683,28 +564,15 @@ missing_data$SS.T1.Gn2=data$Richness[match(paste(missing_data$STUDY_PLOT,missing
 #remove the unique ID column 
 missing_data <- missing_data %>%
   select(-UNIQUE_ID)
-#save
-write.csv(missing_data, "~/Documents/Work and Career/LDP/Working Group/data_missing_richness_ENB_032124.csv")
+
+#save - only for studies 221 and 39
+#If running, update save location 
+#write.csv(missing_data, "~/Documents/Work and Career/LDP/Working Group/data_missing_richness_ENB_032124.csv")
 
 
 missing_data_check <- read.csv("~/Documents/Work and Career/LDP/Working Group/data_missing_richness.csv")
 
-
-#loop through all biomass 
-#results.biomass=data.frame(matrix(ncol=13,nrow=0, dimnames=list(NULL, c("TS_ID","STUDY_PLOT","Gn1", "Gn2", "Log.prop.change.Gn1","Log.prop.change.Gn2","SERIES.n","SERIES.start","SERIES.end","YEAR.T","YEAR.T1","METRIC","SERIES.l")))) #makes an empty dataframe
-#pb<-set.prog.bar(nrow(final.pairs.biomass))
-#for (i in 1:nrow(final.pairs.biomass)){
-#   pb$tick()
-#   
-#   ts=final.pairs.biomass$TS_ID[i]
-#   wk<-get.log.prop.change.biomass(ts)
-#   results.biomass<-rbind(results.biomass,wk)
-#   
-# } #get prop change for every genera pair
-
-#combine
-#results=rbind(results.abundance,results.biomass)
-
+##### IE CODE - adding metadata into results.abundance #####
 #add in metadata
 results.abundance$STUDY_ID<-gsub("\\~.*","",results.abundance$STUDY_PLOT)
 results.abundance$REALM<-metadata$REALM[match(results.abundance$STUDY_ID,metadata$STUDY_ID)]
@@ -721,9 +589,9 @@ dim(results.abundance)
 length(unique(results.abundance$TS_ID)) #527874
 length(unique(results.abundance$STUDY_ID)) #69
 
-#save
-write.csv(results.abundance,"~/Documents/Work and Career/LDP/Working Group/results.abundance_221_39.csv")
-results<-read.csv("/Users/isaaceckert/Desktop/within.study.updated.data.csv")
+#save results.abundance with updated 
+#write.csv(results.abundance,"~/Documents/Work and Career/LDP/Working Group/results.abundance_221_39.csv")
+#results<-read.csv("/Users/isaaceckert/Desktop/within.study.updated.data.csv")
 
 
 table(results$METRIC)
