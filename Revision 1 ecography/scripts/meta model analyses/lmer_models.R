d9274e667aca8aa795bf19a8f30c972174c24012
cour10eygrace <courtneygrace125@gmail.com>
2024-04-26 17:20:59 -0700

    lmer models and predictive accuracy

--- Revision 1 ecography/scripts/meta model analyses/lmer_models.R
+++ Revision 1 ecography/scripts/meta model analyses/lmer_models.R
@@ -1,18 +1,36 @@
-
 library(tidyverse)
 library(lmerTest)
 load(file="Revision 1 ecography/output/prep_data/model_data_final.Rdata")
 
 lmermod<-lmerTest::lmer(z~scale.SERIES.l+ treatment_yn_clean + scale.abs.lat + interaction_present.factor + (1|STUDY_ID) +
                  (1| resolved_taxa_pair),  dat=moddat)
+save(lmermod, file="Revision 1 ecography/output/lmer_model_final.Rdata")
+#model summary and R2 
 summary(lmermod)
 MuMIn::r.squaredGLMM(lmermod)
-save(lmermod, file="Revision 1 ecography/output/lmer_model_final.Rdata")
 
 #model k fold cross validation 
+load(file="Revision 1 ecography/output/lmer_model_final.Rdata")
 library(cv)
-kfoldcv<-cv(lmermod, k=10, )
-kfoldcv_study<-cv(lmermod, clusterVariables="STUDY_ID")
+#fold 10 times with n/10 dataset each time 
+kfoldcv<-cv(lmermod, k=10) 
+save(kfoldcv, file="Revision 1 ecography/output/lmer_model_kfoldstudy.Rdata")
+
+#fold n times (63 study IDs) leaving out 1 study each time 
+kfoldcv_study<-cv(lmermod, clusterVariables="STUDY_ID") 
+save(kfoldcv_study, file="Revision 1 ecography/output/lmer_model_kfoldstudy.Rdata")
+
+#fold n times (16 taxa categories) leaving out 1 category each time 
+kfoldcv_tax<-cv(lmermod, clusterVariables="resolved_taxa_pair")
+save(kfoldcv_tax, file="Revision 1 ecography/output/lmer_model_kfoldtax.Rdata")
+
+#posterior predictive check plots
+library(performance)
+library(see)
+#w random effects
+performance::check_predictions(lmermod, re_formula = NULL)
+#w/o random effects
+performance::check_predictions(lmermod, re_formula = NA)
 
 #add total species # back in 
 
