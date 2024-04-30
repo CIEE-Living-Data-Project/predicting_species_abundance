2c09a2ca9974929eb04df94d36bf0947610e58df
Gavia Lertzman-Lepofsky <gavia.lertzmanlepofsky@mail.utoronto.ca>
2024-04-30 14:32:33 -0400

    updates to figure 5, mansuscript fig script and figure 2

--- Revision 1 ecography/scripts/figures/Manuscript_figures.R
+++ Revision 1 ecography/scripts/figures/Manuscript_figures.R
@@ -11,8 +11,17 @@ library(cowplot)
 # Read in data and model ####
 load(file="Revision 1 ecography/output/prep_data/model_data_final.Rdata")
 moddat <- subset(moddat, STUDY_ID!=39 & STUDY_ID!=221 & STUDY_ID!=225)
-load(file="Revision 1 ecography/output/lmer_model_final.Rdata")
+load(file="Revision 1 ecography/output/meta model/lmer_model_final.Rdata")
 model <- lmermod
+
+
+# Calculate numbers for results ####
+### raw correlations ####
+# calculate
+# use cuttoffs of .15 bc greater than this is a positive correlation as defined by the model
+sum(moddat$cor >= 0.15) #positive = 129018
+nrow(subset(moddat, cor > -0.15 & cor < .15)) #neutral = 132531
+sum(moddat$cor <= -0.15) #negative = 84311
 
 
 # Main text figures ####
@@ -105,17 +114,13 @@ drawWorld<-function(lats) {
                alpha=0.4)) 
 
 
-## Figure 3. model accurary ####
-### raw correlations ####
+##  Figure 3. coef plot  #######
 # histogram of pearson cor
 hist <- moddat %>% 
-  mutate(group=ifelse(cor>=.3, "positive", 
-                      ifelse(cor<=-.3, "negative", "neutral"))) %>% 
+  mutate(group=ifelse(cor>=.15, "positive", 
+                      ifelse(cor<=-.15, "negative", "neutral"))) %>% 
   ggplot(aes(x=cor, fill=group)) + 
   geom_histogram(colour="black", bins=29) +
-  #geom_histogram(aes(y=..count..., fill=group), colour="black") + #, fill="#8DA0CB"
-  #geom_histogram(aes(y=..density..), colour="black", fill="white")+
-  #geom_density(alpha=.2, fill="#8DA0CB") +
   theme_bw()+
   xlab("Correlation")+
   ylab("Number of genus pairs")+
@@ -123,28 +128,22 @@ hist <- moddat %>%
              color="black", linetype="dashed", size=1)+
   theme(panel.grid   = element_blank(),
         axis.ticks.y = element_blank(),
-        axis.text.y  = element_text(hjust = 0),
-        #text = element_text(family = "Ubuntu")
-  ) +
+        axis.text.y  = element_text(hjust = 0)) +
   theme_bw(base_size = 25) +
   theme(panel.grid.major.x = element_blank(),  # Hide major x-axis grid lines
         panel.grid.minor.x = element_blank()) +   # Hide minor x-axis grid lines
   my.theme +
-  scale_fill_manual(values = c("#66C2A5", "#8DA0CB","#FC8D62"))
+  scale_fill_manual(values = c("#66C2A5", "#8DA0CB","#FC8D62")) +
+  theme(legend.position="top",
+        legend.spacing.x = unit(1, 'cm'))+
+  guides(fill = guide_legend(label.position = "top", title="Group"))
 
-ggsave("Revision 1 ecography/output/figures/raw data histogram.pdf", hist, 
-       width=8, height=5, units="in")
-
-
-##  Figure 4. coef plot  #######
-
-# main figure
 # time series and interactions error so small can't see, make insets
 # better to use confidence intervals?
 total <- summary(model)$coefficients %>% 
   data.frame() %>% 
   mutate(term = rownames(.)) %>% 
-  ggplot(aes(Estimate, term, xmin=Estimate-`Std..Error`, xmax=Estimate+`Std..Error`, colour=term)) +
+  ggplot(aes(Estimate, term, xmin=Estimate-2*`Std..Error`, xmax=Estimate+2*`Std..Error`, colour=term)) +
     geom_vline(xintercept = 0, linetype = "dashed", color = "darkgrey", size=2) +
   geom_pointrange(linewidth=2, size=1.8) +
   labs(x = expression("Estimate"),
@@ -158,9 +157,9 @@ total <- summary(model)$coefficients %>%
   my.theme +
   scale_y_discrete(
     labels = c("(Intercept)" = "Intercept",
-      "scale.SERIES.l" = "Time series length",
+      "scale.SERIES.l" = "Time series \nlength",
       "scale.abs.lat" = "Latitude",
-      "interaction_present.factor1" = "GLoBI interactions",
+      "interaction_present.factor1" = "GLoBI \ninteractions",
       "treatment_yn_cleanyes" = "Disturbance"),
     limits = c("scale.SERIES.l", 
                "treatment_yn_cleanyes",
@@ -172,13 +171,11 @@ total <- summary(model)$coefficients %>%
   scale_color_manual(values=c("grey30", "#009E73","#0072B2" ,"#CC79A7","#E69F00" ))
 
 
-# lat inset
-
 time.inset <- summary(model)$coefficients %>% 
   data.frame() %>% 
   mutate(term = rownames(.)) %>% 
   filter(term=="scale.SERIES.l") %>% 
-  ggplot(aes(Estimate, term, xmin=Estimate-`Std..Error`, xmax=Estimate+`Std..Error`, colour=term)) +
+  ggplot(aes(Estimate, term, xmin=Estimate-2*`Std..Error`, xmax=Estimate+2*`Std..Error`, colour=term)) +
   #geom_vline(xintercept = 0, linetype = "dashed", color = "darkgrey", size=2) +
   geom_pointrange(linewidth=2, size=1.2) +
   labs(x = expression(""),
@@ -192,7 +189,7 @@ time.inset <- summary(model)$coefficients %>%
   my.theme +
   scale_y_discrete(labels=c("scale.SERIES.l" = ""),
     limits = c("scale.SERIES.l")) +
-  scale_x_continuous(breaks=c(.0142, 0.0148, .0153)) +
+  scale_x_continuous(breaks=c(.0140, .016)) +
   guides(col = "none") +
   scale_color_manual(values=c("#CC79A7" ))
 
@@ -201,7 +198,7 @@ int.inset <- summary(model)$coefficients %>%
   data.frame() %>% 
   mutate(term = rownames(.)) %>% 
   filter(term=="interaction_present.factor1") %>% 
-  ggplot(aes(Estimate, term, xmin=Estimate-`Std..Error`, xmax=Estimate+`Std..Error`, colour=term)) +
+  ggplot(aes(Estimate, term, xmin=Estimate-2*`Std..Error`, xmax=Estimate+2*`Std..Error`, colour=term)) +
   #geom_vline(xintercept = 0, linetype = "dashed", color = "darkgrey", size=2) +
   geom_pointrange(linewidth=2, size=1.2) +
   labs(x = expression(""),
@@ -217,33 +214,34 @@ int.inset <- summary(model)$coefficients %>%
                    limits = c("interaction_present.factor1")) +
   guides(col = "none") +
   scale_color_manual(values=c("#009E73" )) +
-  scale_x_continuous(breaks=c(.0070, .0083, .0095)) 
+  scale_x_continuous(breaks=c(.006, .011)) 
 
 
-fig4 <- total + 
+total2 <- total + 
   annotation_custom(
     ggplotGrob(time.inset), 
     #xmin = .10, xmax = .2, ymin = .6, ymax = 1.6) +
-    xmin = .12, xmax = .22, ymin = .6, ymax = 1.6) +
+    xmin = .14, xmax = .24, ymin = .6, ymax = 1.6) +
   annotation_custom(
     ggplotGrob(int.inset), 
     #xmin = .05, xmax = .15, ymin = 2.6, ymax = 3.6)
-    xmin = .12, xmax = .22, ymin = 2.6, ymax = 3.6) 
+    xmin = .14, xmax = .24, ymin = 2.6, ymax = 3.6) 
 
-ggsave("Revision 1 ecography/output/figures/figure4.pdf", 
-       fig4, width=15, height=8, units="in")
+fig3 <- plot_grid(hist, total2, rel_widths = c(1,2))
 
-##  Figure 5. ranef plots  #######
+ggsave("Revision 1 ecography/output/figures/figure3.pdf", 
+       fig3, width=20, height=8, units="in")
 
+##  Figure 4. ranef plots  #######
 str(dd <- as.data.frame(ranef(model)))
 ranef_study <- dd %>% 
   filter(grpvar=="STUDY_ID") %>% 
-  ggplot(aes(condval, grp,
-             xmin=condval -1.96*condsd, 
-             xmax=condval + 1.96*condsd)) +
+  ggplot(aes(condval+0.1671776, grp,
+             xmin=condval+0.1671776 -1.96*condsd, 
+             xmax=condval+0.1671776 + 1.96*condsd)) +
   geom_pointrange(linewidth=2, size=1.2) +
-  geom_vline(xintercept = 0, linetype = "dashed", color = "darkgrey")+
-  geom_vline(xintercept = 0.1666366, linetype = "dashed", color = "#8DA0CB", size=2)+
+  #geom_vline(xintercept = 0, linetype = "dashed", color = "darkgrey")+
+  geom_vline(xintercept = 0.1671776, linetype = "dashed", color = "#8DA0CB", size=2)+
   geom_pointinterval() +
   theme(panel.grid   = element_blank(),
         axis.ticks.y = element_blank(),
@@ -259,12 +257,12 @@ ranef_study <- dd %>%
 
 ranef_taxa <- dd %>% 
   filter(grpvar=="resolved_taxa_pair") %>% 
-  ggplot(aes(condval, grp,
-             xmin=condval -1.96*condsd, 
-             xmax=condval +1.96*condsd)) +
+  ggplot(aes(condval+0.1671776, grp,
+             xmin=condval+0.1671776 -1.96*condsd, 
+             xmax=condval+0.1671776 +1.96*condsd)) +
   geom_pointrange(linewidth=2, size=1.2) +
-  geom_vline(xintercept = 0, linetype = "dashed", color = "darkgrey")+
-  geom_vline(xintercept = 0.1666366, linetype = "dashed", color = "#8DA0CB", size=2)+
+  #geom_vline(xintercept = 0, linetype = "dashed", color = "darkgrey")+
+  geom_vline(xintercept = 0.1671776, linetype = "dashed", color = "#8DA0CB", size=2)+
   geom_pointinterval() +
   theme(panel.grid   = element_blank(),
         axis.ticks.y = element_blank(),
@@ -295,105 +293,64 @@ ranef_taxa <- dd %>%
                "Magnoliopsida.Gnetopsida",
                "Gnetopsida.Monocots")) 
 
-fig5 <- plot_grid(ranef_study, ranef_taxa)
-ggsave("Revision 1 ecography/output/figures/figure5.pdf", fig5, width=20, height=20, units="in")
+fig4 <- plot_grid(ranef_study, ranef_taxa)
+ggsave("Revision 1 ecography/output/figures/figure4.pdf", fig4, width=20, height=20, units="in")
 
-##  Figure 6. linear predictor plots  #######
-# 4x4 panel plot highlighting random effect from 
-# Monocots.Monocots
-# Aves.Aves
-# Insecta.Insecta
-# Pinopsida.Pinopsida
-# for an average site
+## Figure 5 posterior pred plots ####
+library(performance)
 
-# predict new data across time series length
-# disturbance=0,1, interaction=0, latitude=0
+# w/o random effects
+fig5a_dat <- performance::check_predictions(lmermod, re_formula = NA) %>% 
+  pivot_longer(cols=c(sim_1:y)) %>% 
+  mutate(Grouping = ifelse(name=="y", "Observed \ndata", "Model-predicted \ndata"))
 
-new.data <- expand.grid(scale.SERIES.l = seq(min(moddat$scale.SERIES.l), 
-                                                     max(moddat$scale.SERIES.l), by = .1),
-                        scale.abs.lat = 0,
-                        interaction_present.factor=c("0","1"),
-                        treatment_yn_clean= c("no", "yes"), 
-                        resolved_taxa_pair=c("Aves.Aves", "Monocots.Monocots", "Insecta.Insecta", "Pinopsida.Pinopsida"), 
-                        STUDY_ID=c("54") # chosen bc doesn't exert strong effect, close to zero
-                        ) 
-new.predict <- cbind(predict(model, newdata=new.data), new.data)
-
-# unscale time.series length
-unscale <-  function (x, x.avg, s) { # x is transformed variable, x.avg is mean before scale and s is sd before scale
-  x*s + x.avg
-}
-
-# re-order facets
-int_names <- c(
-  "0" = "No GLoBI interactions",
-  "1" = "GLoBI interactions", 
-  "Aves.Aves"="Aves.Aves", 
-  "Monocots.Monocots"="Monocots.Monocots", 
-  "Insecta.Insecta"="Insecta.Insecta", 
-  "Pinopsida.Pinopsida"="Pinopsida.Pinopsida")
-
-
-fig6 <- new.predict %>% 
-  mutate(SERIES.l = unscale(scale.SERIES.l, mean(moddat$SERIES.l), sd(moddat$SERIES.l))) %>% 
-  ggplot() +
-  geom_line(aes(SERIES.l, `predict(model, newdata = new.data)`, colour=treatment_yn_clean), 
-            size=1.2) + 
-  # error
-  # geom_ribbon(aes(SERIES.l, `predict(model, newdata = new.data)`, ymin = linepred-linepred.error, ymax = linepred+linepred.error,
-  #                 group = treatment_yn_clean, fill = treatment_yn_clean), alpha=.2,
-  #             show.legend = F) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "#5E4141", size=1.1)+
-  facet_grid(interaction_present.factor~factor(resolved_taxa_pair, 
-                                               levels=c("Aves.Aves","Insecta.Insecta", "Monocots.Monocots", "Pinopsida.Pinopsida")), 
-             labeller = as_labeller(int_names)) +
-  theme(panel.grid   = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.y  = element_text(hjust = 0)) +
+fig5a <- ggplot(fig5a_dat, 
+               aes(value, col=as.factor(Grouping), group=as.factor(name))) + 
+  geom_density(size=1.5) +
   theme_bw(base_size = 25) +
   theme(panel.grid.major.x = element_blank(),  # Hide major x-axis grid lines
-        panel.grid.minor.x = element_blank(),
-        strip.background = element_rect(colour="white", fill="white")) +   # Hide minor x-axis grid lines
+        panel.grid.minor.x = element_blank()) +   # Hide minor x-axis grid lines
   my.theme +
-  labs(x = "Time series length (years)",
-       y = "Estimate", 
-       color = "Disturbance") +
-  scale_color_manual(values=c( "#004A39", "#E69F00")) + 
-  scale_fill_manual(values=c( "#004A39", "#E69F00"))   
-fig6
+  labs(x = "Value",
+       y = "Z-score") +
+  scale_colour_manual(name = "", 
+                      values=c("#8DA0CB","#FC8D62")) +
+  theme(legend.position = "none")
 
-ggsave("Revision 1 ecography/output/figures/figure6.pdf", fig6, width=20, height=10, units="in")
+
+# w random effects
+fig5b_dat <- performance::check_predictions(model, re_formula = NULL) %>% 
+  pivot_longer(cols=c(sim_1:y)) %>% 
+  mutate(Grouping = ifelse(name=="y", "Observed \ndata", "Model-predicted \ndata"))
+
+fig5b<- ggplot(fig5b_dat, 
+       aes(value, col=as.factor(Grouping), group=as.factor(name))) + 
+  geom_density(size=1.5) +
+  theme_bw(base_size = 25) +
+  theme(panel.grid.major.x = element_blank(),  # Hide major x-axis grid lines
+        panel.grid.minor.x = element_blank()) +   # Hide minor x-axis grid lines
+  my.theme +
+  labs(x = "Value",
+       y = "") +
+scale_colour_manual(name = "", 
+                    values=c("#8DA0CB","#FC8D62"))
+
+fig5 <- plot_grid(fig5a, fig5b)
+
+ggsave("Revision 1 ecography/output/figures/figure5.pdf", fig5, width=20, height=10, units="in")
 
 # Tables ####
 ## Table S3 ####
-model %>% 
-  spread_draws(b_Intercept, b_scale.SERIES.l, b_scale.abs.lat , b_treatment_yn_cleanyes, b_interaction_present.factor1,
-               sd_STUDY_ID__Intercept, sd_resolved_taxa_pair__Intercept) %>% 
-  #median_qi() %>% 
-  summarise_draws() %>% 
+summary(model)$coefficients %>% 
   knitr::kable(format="simple", digits=c(6,6,6,6,6,6,6,3,0,0))
 
 ## Table S4 ####
 # study ID
-model %>%
-  spread_draws(r_STUDY_ID[STUDY_ID, ]) %>%
-  summarise_draws() #%>% 
-  #knitr::kable(format="simple", digits=c(6,6,6,6,6,6,6,3,0,0))
-
 # taxonomic category
-model %>%
-  spread_draws(r_resolved_taxa_pair[resolved_taxa_pair, ]) %>%
-  summarise_draws() %>% 
+as.data.frame(ranef(model)) %>% 
+  mutate(on.mean = condval+0.1671776) %>% 
   knitr::kable(format="simple", digits=c(6,6,6,6,6,6,6,3,0,0))
 
-# can also look at taxonomic category per study ID
-model %>%
-  spread_draws(r_STUDY_ID[STUDY_ID, ], r_resolved_taxa_pair[resolved_taxa_pair, ]) %>%
-  summarise_draws() %>% 
-  knitr::kable(format="simple", digits=c(6,6,6,6,6,6,6,3,0,0))
-
-#write.csv(tableS4, "Revision 1 ecography/output/figures/tableS4.csv")
-  
 
 
 # Sup mat figures ####
